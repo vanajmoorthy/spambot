28b2e19b0f86a364b524380cb092e91d199c082c
Vanaj Moorthy <vanajmoorthy@gmail.com>
2019-08-26 23:19:12 +0530

    fuxed syntax error

--- spam.py
+++ spam.py
@@ -1,4 +1,4 @@
-`from selenium import webdriver
+from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
